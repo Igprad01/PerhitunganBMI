4fe4718abe1a670688362b63696702a19815b916
igprad01 <igustiputra99@gmail.com>
2024-02-20 20:59:15 +0700

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,15 @@
+function inputBerat() {
+    const berat = parseFloat(document.getElementById('beratBadan').value);
+    const tinggi = parseFloat(document.getElementById('tinggiBadan').value) / 100; 
+    let totalBerat = berat / (tinggi * tinggi);
+
+    if (totalBerat < 18.5) {
+        alert(`Anda kurus dengan berat badan ${totalBerat.toFixed(2)}`);
+    } else if (totalBerat >= 18.5 && totalBerat < 25) {
+        alert(`Anda ideal dengan berat badan ${totalBerat.toFixed(2)}`);
+    } else if (totalBerat >= 25) {
+        alert(`Anda obesitas dengan berat badan ${totalBerat.toFixed(2)}`);
+    } else {
+        alert("Null");
+    }
+}
