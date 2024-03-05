7069545700689ec598d804d616094c64a0835ccb
igprad01 <igustiputra99@gmail.com>
2024-03-05 11:02:48 +0700

    add textcontent textarea

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,15 @@
 function inputBerat() {
     const berat = parseFloat(document.getElementById('beratBadan').value);
     const tinggi = parseFloat(document.getElementById('tinggiBadan').value) / 100; 
-    const laki = document.getElementById('lakiLaki');
-    const perempuan = document.getElementById('perempuan');
+    const output = document.getElementById('output');
     let totalBerat = berat / (tinggi * tinggi);
 
     if (totalBerat < 18.5) {
-        alert(`Anda kurus dengan berat badan ${totalBerat.toFixed(2)}`);
+        output.textContent = `anda kurus dengan berat badan ${totalBerat.toFixed(2)}`;
     } else if (totalBerat >= 18.5 && totalBerat < 25) {
-        alert(`Anda ideal dengan berat badan ${totalBerat.toFixed(2)}`);
+        output.textContent = `anda kurus dengan berat badan ${totalBerat.toFixed(2)}`;
     } else if (totalBerat >= 25) {
-        alert(`Anda obesitas dengan berat badan ${totalBerat.toFixed(2)}`);
+        output.textContent = `anda kurus dengan berat badan ${totalBerat.toFixed(2)}`;
     } else {
         alert("Null");
     }
